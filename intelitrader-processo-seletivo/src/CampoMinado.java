4f03e45df0b9e882ffb70e0486c01c1be32701cc
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-22 13:36:18 -0300

    Exercício terminado

---
+++ intelitrader-processo-seletivo/src/CampoMinado.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CampoMinado {
+
+	int matriz;
+	int contador = 0;
+	
+	Scanner entrada = new Scanner(System.in);
+	
+	public void digiteTamanhoCampo() {
+		System.out.print("Digite o número da matriz: ");
+		this.matriz = entrada.nextInt();
+		
+		for (int l = 0; l < this.matriz; l++) {
+			for (int c = 0; c < this.matriz; c++) {
+				if (contador % 8 == 0) {
+					System.out.print("*");
+					
+				} else {
+					System.out.print(".");
+				}
+				contador++;
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		CampoMinado campo = new CampoMinado();
+		campo.digiteTamanhoCampo();
+	}
+}
